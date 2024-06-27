6946a47724bf1b533968aeaf12ec2f6191eb04b0
Temitope Douglas <taymeedouglas07@gmail.com>
2024-06-27 22:23:57 +0100

    Day2
    updated the app and index file.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,9 @@
 import React from 'react'
 import Navbar from './components/Navbar/Navbar'
+import { Route, Routes } from 'react-router-dom'
+import Home from './pages/Home/Home'
+import Cart from './pages/Cart/Cart'
+import PlaceOrder from './pages/PlaceOrder/PlaceOrder'
 
 const App = () => {
   return (
@@ -7,6 +11,16 @@ const App = () => {
     <div className='app'>
       {/* import the navbar component */}
       <Navbar/>
+      
+      {/* within this route we will create multiple routes */}
+      <Routes>
+        <Route path='/' element={<Home/>} />
+        <Route path='/Cart' element={<Cart/>} />
+        <Route path='/PlaceOrder' element={<PlaceOrder/>} />
+        
+      </Routes>
+
+      {/* setup the routes */}
     </div>
   )
 }
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,14 +4,19 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+// import the bowser router
+import { BrowserRouter} from 'react-router-dom'
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+
+  // wrap the App in the browser router
+  <BrowserRouter>
+   <App />
+  </BrowserRouter>
+   
+  
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+
 reportWebVitals();
