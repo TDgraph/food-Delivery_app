94f625e0081b6e90df310c6746cac904110c0984
Temitope Douglas <taymeedouglas07@gmail.com>
2024-06-27 22:01:46 +0100

    Day2
    Created the pages (cart, home and place-order).
    Added the state variable for the menu buttons using the onclick feature.

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -1,8 +1,11 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Navbar.css'
 import { assets } from '../../Assets/assets'
 
 const Navbar = () => {
+
+    // create the state variable
+    const [menu, setMenu] = useState("menu");
   return (
     <div className='navbar'>
         {/* import the logo image from the asset.js folder */}
@@ -10,10 +13,12 @@ const Navbar = () => {
 
       {/* create the navigation bar */}
       <ul className="navbar-menu">
-        <li>Home</li>
-        <li>Menu</li>
-        <li>Mobile-app</li>
-        <li>Contact Us</li>
+
+      {/* add the onclick menu to enable the underline feature move between the menu button when being clicked */}
+        <li onClick={()=>setMenu("home")} className={menu==="home"?"active":""}>Home</li>
+        <li onClick={()=>setMenu("menu")} className={menu==="menu"?"active":""}>Menu</li>
+        <li onClick={()=>setMenu("mobile-app")} className={menu==="mobile-app"?"active":""}>Mobile-app</li>
+        <li onClick={()=>setMenu("contact-us")} className={menu==="contact-us"?"active":""}>Contact Us</li>
       </ul>
 
       {/* import the search icon and basket icon from the assets folder */}
