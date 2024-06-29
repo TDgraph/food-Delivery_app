f39609888613a261d2a379bee0464c8b6e5d8edc
Temitope Douglas <taymeedouglas07@gmail.com>
2024-06-29 06:21:01 +0100

    updated
    Testing the push state

---
+++ frontend/src/components/Header/Header.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './Header.css'
+
+const Header = () => {
+  return (
+    <div className='header'>
+        <div className="header-content">
+            <h2>Order your favourite food here</h2>
+            <p>choose from a diverse menu with alot of delecable and sprcially crafted meals to suit your taste </p>
+            <button>View Menu</button>
+        </div>
+      
+    </div>
+  )
+}
+
+export default Header
